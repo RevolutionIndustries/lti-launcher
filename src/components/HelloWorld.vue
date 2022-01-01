cf7e685a2feb99f01f383f9d741b84b6fbc95965
iturgeon <iturgeon@gmail.com>
2022-01-01 15:02:42 -0500

    vue version launches correctly

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="hello">
-    <h1>LTI Rocket Launcher</h1>
+    <h1>LTI Launchpad</h1>
     <p>
       Open source tools for developers and admins working with Learning Tools Interoperability systems.
     </p>
+
     <div class="setup">
-      <h3>Setup</h3>
+      <h3>Setup Integration</h3>
       <ul>
-        <li>LTI KEY: <input v-model="ltiKey" placeholder="enter an lti key" /></li>
-        <li>LTI SECRET: <input v-model="ltiSecret" placeholder="enter an lti secret" /></li>
+        <li>BASE URL: <input v-model="baseURL" placeholder="base url like https://site.com" /></li>
         <li>LAUNCH URL: <input v-model="launchURL" placeholder="url to send lti launch to" /></li>
         <li>COURSE NAV URL: <input v-model="courseNavUrl" placeholder="url to send courseNav launch to" /></li>
         <li>SELECTION URL: <input v-model="resourceSelectionUrl" placeholder="" /></li>
+        <li>RESOURCE LINK ID: <input v-model="resourceLinkId" placeholder="" /></li>
+        <li>LTI KEY: <input v-model="ltiKey" placeholder="enter an lti key" /></li>
+        <li>LTI SECRET: <input v-model="ltiSecret" placeholder="enter an lti secret" /></li>
+      </ul>
+      <h4>Setup User</h4>
+      <ul>
         <li>EMAIL: <input v-model="email" placeholder="user email" /></li>
         <li>NAME: <input v-model="familyName" placeholder="user last name" /></li>
         <li>FULL NAME: <input v-model="fullName" placeholder="user full name" /></li>
@@ -19,39 +25,72 @@
         <li>SOURCEDID: <input v-model="sourcedId" placeholder="rand:555" /></li>
         <li>ROLES: <input v-model="roles" placeholder="Instructor" /></li>
         <li>USERID: <input v-model="userId" placeholder="555" /></li>
-
         https://www.imsglobal.org/spec/lti/v1p3/#resource-link
       </ul>
-      <button @click="randomizeUser({student: false})">Random Instructor</button>
-      <button @click="randomizeUser({student: true})">Random Student</button>
+      <h3>Generate Random User</h3>
+      <button @click="randomizeUser({student: false})">Instructor</button>
+      <button @click="randomizeUser({student: true})">Student</button>
     </div>
+
     <h3>Launch</h3>
     <ul>
 			<li><b>LTI Course Nav:</b> (simulate launch from LMS nav menu)
 				<ul>
-					<li><a href="#" onClick="clickLink(COURSE_NAV, 'course_1', false, true);">as an Instructor</a></li>
-					<li><a href="#" onClick="clickLink(COURSE_NAV, 'course_1');">as a Student</a></li>
+					<li><button @click="clickLink(COURSE_NAV, 'course_1');">Launch from Course Nav</button></li>
+          <li><button @click="clickLink(RESOURCE_SELECTION, 'course_1');">Launch Resource Selection</button></li>
+          <li><button @click="clickLink(ASSIGNMENT, 'course_1');">Launch Assignment</button></li>
 				</ul>
 			</li>
     </ul>
+
     <h3>By us. For Everyone.</h3>
     <p>
       Created by OpenEduCloud &amp; the founding people behind Obojoob &amp; Materia.
     </p>
+
+    <form ref="form" v-bind:method="method" v-bind:action="endpoint" v-bind:target="launchURL">
+        <input
+          v-for="(value, name) in params"
+          v-bind:key="name"
+          type="hidden"
+          v-bind:name="name"
+          v-bind:value="value"
+        />
+    </form>
+
   </div>
 </template>
 
 <script>
+const sig = require('oauth-signature')
 const COURSE_NAV = 'course_navigation'
 const ASSIGNMENT = 'assignment'
 const RESOURCE_SELECTION = 'resource_selection'
-const syncKeys = ['ltiKey','ltiSecret','launchURL','courseNavUrl','resourceSelectionUrl','email','familyName','fullName','givenName','sourcedId','roles','userId']
+const syncKeys = ['ltiKey','ltiSecret','baseURL','resourceLinkId','launchURL','courseNavUrl','resourceSelectionUrl','email','familyName','fullName','givenName','sourcedId','roles','userId']
+
+const ltiContext = {
+	context_id: 'S3294476',
+	context_label: 'OBO4321',
+	context_title: 'Obojobo Local Dev 101',
+	context_type: 'CourseSection'
+}
+
+const ltiToolConsumer = {
+	tool_consumer_info_product_family_code: 'obojobo-next',
+	tool_consumer_instance_guid: 'obojobo.ucf.edu',
+	tool_consumer_instance_name: 'University of Central Florida',
+	tool_consumer_instance_url: 'https://obojobo.ucf.edu/'
+}
+
 export default {
   name: 'HelloWorld',
   data: () => ({
+    method: 'POST',
     ltiKey: '',
     ltiSecret: '',
+    baseURL: '',
     launchURL: '',
+    resourceLinkId: '',
     courseNavUrl: '',
     resourceSelectionUrl: '',
     email: '',
@@ -60,10 +99,18 @@ export default {
     givenName: '',
     sourcedId: '',
     roles: '',
-    userId: ''
+    userId: '',
+    endpoint: null,
+    params: {}
   }),
   props: {
     msg: String
+  },
+  created() {
+    // establish some constants for use in the template
+    this.COURSE_NAV = COURSE_NAV
+    this.ASSIGNMENT = ASSIGNMENT
+    this.RESOURCE_SELECTION = RESOURCE_SELECTION
   },
   mounted() {
     // when mounting, load each key from local storage into scope
@@ -78,8 +125,14 @@ export default {
     ltiSecret(newVal){
       localStorage.ltiSecret = newVal;
     },
+    baseURL(newVal){
+      localStorage.baseURL = newVal;
+    },
     launchURL(newVal){
       localStorage.launchURL = newVal;
+    },
+    resourceLinkId(newVal){
+      localStorage.resourceLinkId = newVal
     },
     courseNavUrl(newVal){
       localStorage.courseNavUrl = newVal;
@@ -110,6 +163,40 @@ export default {
     }
   },
   methods: {
+    submit(){
+      const oauthParams = {
+        oauth_nonce: Math.round(new Date().getTime() / 1000.0),
+        oauth_timestamp: Math.round(new Date().getTime() / 1000.0),
+        oauth_callback: 'about:blank',
+        oauth_consumer_key: this.ltiKey,
+        oauth_signature_method: 'HMAC-SHA1',
+        oauth_version: '1.0'
+      }
+
+      const personParams = {
+        lis_person_contact_email_primary: this.email,
+        lis_person_name_family: this.familyName,
+        lis_person_name_full: this.fullName,
+        lis_person_name_given: this.givenName,
+        lis_person_sourcedid: this.sourcedId,
+        roles: this.roles,
+        user_id: this.userId,
+        user_image: 'https://s.gravatar.com/avatar/17f34572459fa620071cae55d7f1eacb?s=80'
+      }
+
+      let params = {...this.params, ...personParams, ...oauthParams}
+      const hmac_sha1 = sig.generate(this.method, this.endpoint, params, this.ltiSecret, '', {encodeSignature: false})
+
+      params.oauth_signature = hmac_sha1
+      this.params = {...params, ...oauthParams}
+
+      // render to form then submit
+      this.$nextTick(() => {
+          this.$refs.form.submit()
+          console.log(this.params)
+      });
+
+    },
     randomizeUser({student = false}) {
       const randomIntString = Math.floor(Math.random() * Math.floor(999999999)) + ''
       const typeString = student ? 'Learner' : 'Instructor'
@@ -122,44 +209,81 @@ export default {
       localStorage.roles = this.roles = typeString
       localStorage.userId = this.userId = randomIntString
     },
-    clickLink(mode, course, scoreImport = false, isInstructor = false) {
-      let modeUrl
-      let launchInFrame = false
-      let url
+    clickLink(mode, course, scoreImport = false) {
+      // let modeUrl
+      // let launchInFrame = false
       switch(mode){
         case ASSIGNMENT:
-          modeUrl = '/launch'
-          url = document.getElementById('launch_url').value
+          {
+            this.endpoint = this.baseURL + this.launchURL
+            const extraParams = {
+              lis_outcome_service_url: 'https://example.fake/outcomes/fake',
+              lti_message_type: 'basic-lti-launch-request',
+              lti_version: 'LTI-1p0',
+              resource_link_id: this.resourceLinkId,
+              score_import: true
+            }
+            this.params = {...ltiContext, ...extraParams}
+          }
           break;
 
         case COURSE_NAV:
-          modeUrl = '/course_navigation'
-          url = document.getElementById('course_nav_url').value
+          {
+            this.endpoint = this.baseURL + this.courseNavUrl
+            // eslint-disable-next-line no-case-declarations
+            const extraParams = {
+              launch_presentation_css_url: 'https://example.fake/nope.css',
+              launch_presentation_document_target: 'frame',
+              launch_presentation_locale: 'en-US',
+              launch_presentation_return_url: 'https://example.fake/fake-return.html',
+              lis_course_offering_sourcedid: 'DD-ST101',
+              lis_course_section_sourcedid: 'DD-ST101:C1',
+              lis_outcome_service_url: 'https://example.fake/outcomes/fake',
+              lis_result_sourcedid: 'UzMyOTQ0NzY6Ojo0Mjk3ODUyMjY6OjoyOTEyMw==',
+              lti_message_type: 'basic-lti-launch-request',
+              lti_version: 'LTI-1p0',
+              resource_link_id: this.resourceLinkId,
+              resource_link_title: 'Phone home'
+            }
+            this.params = {...ltiContext, ...ltiToolConsumer, ...extraParams}
+          }
           break;
 
         case RESOURCE_SELECTION:
-          modeUrl = '/resource_selection'
-          url = document.getElementById('course_resource_selection_url').value
-          launchInFrame = true
+          {
+            this.endpoint = this.baseURL + this.resourceSelectionUrl
+
+            const extraParams = {
+              accept_copy_advice: 'false',
+              accept_media_types: '*/*',
+              accept_multiple: 'false',
+              accept_presentation_document_targets: 'embed,frame,iframe,window,popup,overlay,none',
+              accept_unsigned: 'false',
+              auto_create: 'true',
+              can_confirm: 'false',
+              content_item_return_url: `${location.protocol}//${location.host}/lti/dev/return/resource_selection`,
+              launch_presentation_css_url: 'https://example.fake/nope.css',
+              launch_presentation_locale: 'en-US',
+              lti_message_type: 'ContentItemSelectionRequest',
+              lti_version: 'LTI-1p0',
+              data: "this opaque 'data' should be sent back to the LMS!"
+            }
+            this.params = {...ltiContext, ...ltiToolConsumer, ...extraParams}
+          }
           break;
       }
+      console.log({scoreImport, course})
+      this.submit()
+      // params.append('score_import', scoreImport ? 'true' : 'false')
 
-      const params = new URLSearchParams()
-      params.append('url', url)
-      params.append('is_instructor', isInstructor ? 'true' : 'false')
-      params.append('resource_link_id', course)
-      params.append('score_import', scoreImport ? 'true' : 'false')
-      params.append('lti_key', this.ltiKey)
-      params.append('lti_secret', this.ltiSecret)
+      // if(launchInFrame){
+        // const iframeEl = document.getElementById('the-iframe')
+        // iframeEl.src = window.location.origin+modeUrl+'?'+params.toString();
+        // iframeEl.scrollIntoView()
+      // } else {
+      // window.location.href = window.location.origin+modeUrl+'?'+params.toString();
 
-      console.log({launchInFrame})
-      if(launchInFrame){
-        const iframeEl = document.getElementById('the-iframe')
-        iframeEl.src = window.location.origin+modeUrl+'?'+params.toString();
-        iframeEl.scrollIntoView()
-      } else {
-        window.location.href = window.location.origin+modeUrl+'?'+params.toString();
-      }
+      // }
     }
   }
 }
@@ -183,12 +307,6 @@ ul {
 .setup ul{
   width:100%;
   text-align: left;
-}
-
-.setup li {
-
-  /* display: inline-block; */
-  /* margin: 0 10px; */
 }
 
 .setup input {
